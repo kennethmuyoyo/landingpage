0cc6bc1fdc513afd028f1eb0961213dfbde80f03
Kenneth <kennethmuyoyo@gmail.com>
2023-06-05 11:21:03 +0300

    copychange

--- src/components/LeftCol.tsx
+++ src/components/LeftCol.tsx
@@ -21,8 +21,7 @@ function LeftCol({onClick}:{onClick:()=>void}) {
         </h1>
       </motion.div>
       <motion.div {...headContentAnimation} className="leading-normal font-league-spartan lg:text-xl mb-4 mt-8 text-center lg:text-justify slide-in-bottom-subtitle font-normal text-gray-600 text-base space-y-4">
-        <p>Discover Your <strong>Digital Islamic Guide!</strong></p>
-        <p><span style={{color: 'green'}}>Brother AI.DAM</span> and <span style={{color: 'green'}}>Sister AI.SHA</span>. <br /> Whether you seek urgent answers, need guidance on permissible actions in Islam, or simply require a friendly chat, we are here for you.</p>
+        <p>Discover <span style={{color: 'green'}}>Brother AI.DAM</span> and <span style={{color: 'green'}}>Sister AI.SHA they are here to help you</span>. <br /> Whether you seek urgent answers, need guidance on permissible actions in Islam, or simply require a friendly chat, we are here for you.</p>
         <MobileRightCol /> {/* The image is placed independently outside the paragraph content */}
         <p><span style={{fontWeight: 'bold'}}>Navigating the path of conversion? Desiring to add more insight as a Muslim? Or simply yearning for a handy tool to keep a tally of your daily good deeds? Your quest ends here. This app is custom-built for every Muslim&apos;s unique spiritual journey.</span></p>
         <p>Let&apos;s embark on this journey of faith and together we can <span style={{color: 'green'}}>become better Muslims.</span></p> 
